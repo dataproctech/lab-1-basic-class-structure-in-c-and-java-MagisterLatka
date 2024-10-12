453ed2cf83170aa05cb6ad5b9b0750271013b91b
Michał <michallatka3@wp.pl>
2024-10-12 20:00:02 +0000

    Completed other tasks

--- Car.java
+++ Car.java
@@ -2,19 +2,31 @@ public class Car {
     String make_;
     String model_;
     int year_;
+    int currentYear_;
     
-    public Car(String make, String model, int year) {
+    public Car(String make, String model, int year, int currentYear) {
         make_ = make;
         model_ = model;
         year_ = year;
+        currentYear_ = currentYear;
     }
 
     public void displayInfo() {
         System.out.println("Make: " + make_ + ", model: " + model_ + ", year: " + year_);
     }
 
+    public int getCarAge() {
+        return currentYear_ - year_;
+    }
+
+    public boolean isSameCar(Car other) {
+        return make_ == other.make_ && model_ == other.model_ && year_ == other.year_;
+    }
+
     public static void main(String[] args) {
-        Car car = new Car("Toyota", "Civic", 2024);
+        Car car = new Car("Toyota", "Civic", 2020, 2024), car2 = new Car("Toyota", "Civic", 2020, 2024);
         car.displayInfo();
+        System.out.println("Car age: " + car.getCarAge());
+        System.out.println("Is same car: " + car.isSameCar(car2));
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,21 +3,33 @@
 
 class Car {
     std::string make_, model_;
-    int year_;
+    int year_, currentYear_;
 public:
-    Car(const std::string& make, const std::string& model, int year)
-        : make_(make), model_(model), year_(year) {}
+    Car(const std::string& make, const std::string& model, int year, int currentYear)
+        : make_(make), model_(model), year_(year), currentYear_(currentYear) {}
 
     void displayInfo() const {
         std::cout << "Make: " << make_ << ", model: " << model_ << ", year: " << year_ << std::endl;
     }
-   
+
+    int getCarAge() const {
+        return currentYear_ - year_;
+    }
+
+    bool isSameCar(const Car& other) const {
+        return make_ == other.make_ && model_ == other.model_ && year_ == other.year_;
+    }
+    bool operator==(const Car& other) const {
+        return isSameCar(other);
+    }
 };
 
 int main() {
-    Car car("Toyota", "Civic", 2024), notCar("", "", 0);
+    Car car("Toyota", "Civic", 2020, 2024), notCar("", "", 0, 2024);
     car.displayInfo();
     notCar.displayInfo();
+    std::cout << "Car age: " << car.getCarAge() << std::endl;
+    std::cout << "Is same car?: " << std::boolalpha << car.isSameCar(notCar) << std::endl;//(car == notCar)
     
     return 0;
 }
